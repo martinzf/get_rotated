02bb52d9ff119de5c423167890d219cb1335e2b8
Martin Zapata <martzapa@ucm.es>
2023-01-24 18:19:33 +0100

    Efficiency improvements and prepared in frame

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import scipy.special as sp
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
+# Time step
+dt = .02
+
 def request_float(prompt: str, positive: bool) -> float:
     # Gets user input
     while True:
@@ -33,116 +36,131 @@ w0 = []
 for i in range(1, 4):
     w0.append(request_float(f'w{i} (rad/s): ', False))
 
-# Duration
-t = np.arange(
-    0, 
-    request_float('Input simulation duration (s): ', False),
-    .02)
-
-if all(i != I[0] for i in I[1:]): # Assymetric
+if (w0.count(0) == 2) or all(i == I[0] for i in I): # w along principal axis or spherical symmetry
+    t = np.arange(0, 3, dt)
+    w0 = np.reshape(w0, (3, 1))
+    w = np.tile(w0, len(t))
+elif all(i != I[0] for i in I[1:]): # Assymetric I
     # Correct ordering of basis
     j = np.argsort(I)[len(I) // 2]
     k = (j + 1) % 3
     i = (j + 2) % 3
-    if (w0.count(0) == 2): # w along an axis (2 axis unstable!!)
-        w0 = np.reshape(w0, (3, 1))
-        w = np.tile(w0, len(t))  
-    else:  
-        # Conserved quantities
-        l = np.sum([(I[i] * w0[i]) ** 2 for i in range(3)]) # L^2
-        e = np.sum([I[i] * w0[i] ** 2 for i in range(3)]) # 2T
-        # Change of variables
-        tau = t * np.sqrt( (I[i] * e - l) * (I[j] - I[k]) / (np.prod(I)))
-        # Initial conditions
-        s0 = w0[j] * np.sqrt(I[j] * (I[j] - I[k]) / (l - e * I[k]))
-        m = (I[i] - I[j]) * (l - I[k] * e) / ((I[j] - I[k]) * (I[i] * e - l))
-        tau += sp.ellipkinc(np.arcsin(s0), m)
-        # Solution
-        w = np.empty((3, len(t)))
-        sn, cn, dn, _ = sp.ellipj(tau, m)
-        w[i] = np.sqrt((l - I[k] * e) / (I[i] * (I[i] - I[k]))) * cn  
-        w[j] = np.sqrt((l - I[k] * e) / (I[j] * (I[j] - I[k]))) * sn  
-        w[k] = np.sqrt((I[i] * e - l) / (I[k] * (I[i] - I[k]))) * dn 
-elif all(i == I[0] for i in I): # Spherically symmetric
-    w0 = np.reshape(w0, (3, 1))
-    w = np.tile(w0, len(t))
-else: # Axially symmetric
+    # Conserved quantities
+    l = np.sum([(I[i] * w0[i]) ** 2 for i in range(3)]) # L^2
+    e = np.sum([I[i] * w0[i] ** 2 for i in range(3)]) # 2T
+    # Change of variables
+    m = (I[i] - I[j]) * (l - I[k] * e) / ((I[j] - I[k]) * (I[i] * e - l))
+    K = 4 * sp.ellipk(m)
+    conversion = np.sqrt( (I[i] * e - l) * (I[j] - I[k]) / (np.prod(I)))
+    t = np.arange(0, K / conversion, dt)
+    tau = t * conversion
+    # Initial conditions
+    s0 = w0[j] * np.sqrt(I[j] * (I[j] - I[k]) / (l - e * I[k]))
+    tau += sp.ellipkinc(np.arcsin(s0), m)
+    # Solution
+    w = np.empty((3, len(t)))
+    sn, cn, dn, _ = sp.ellipj(tau, m)
+    w[i] = np.sqrt((l - I[k] * e) / (I[i] * (I[i] - I[k]))) * cn  
+    w[j] = np.sqrt((l - I[k] * e) / (I[j] * (I[j] - I[k]))) * sn  
+    w[k] = np.sqrt((I[i] * e - l) / (I[k] * (I[i] - I[k]))) * dn 
+else: # Axially symmetric I
     # Correct ordering of basis
     k = np.argmax(np.abs(I-np.median(I))) 
     i = (k + 1) % 3
     j = (k + 2) % 3
-    w = np.empty((3, len(t)))
     # Precession of w around symmetry axis (in body frame)
     O = (I[k] - I[i]) / I[i] 
+    t = np.arange(0, 2 * np.pi / O, dt)
+    w = np.empty((3, len(t)))
     w[i] = np.cos(O * t) * w0[i] - np.sin(O * t) * w0[j]
     w[j] = np.sin(O * t) * w0[i] + np.cos(O * t) * w0[j]
     w[k] = w0[k] 
 # Angular momentum
 L = np.reshape(I, (3, 1)) * w
+w_inertial = w
 
 fig = plt.figure(figsize=(10, 10))
-ax1 = fig.add_subplot(2, 1, 1, projection='3d')
-ax2 = fig.add_subplot(2, 1, 2)
+ax1 = fig.add_subplot(2, 2, 1, projection='3d')
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3, projection='3d')
+ax4 = fig.add_subplot(2, 2, 4)
 
-# Axis 1
+# Axes 1 and 3
 ax1.set_title('Body frame', fontdict={'size': 15})
-lim1 = np.max([w, L])
+lim1 = np.max(np.abs([w, L]))
 ax1.set_xlim([-lim1, lim1])
 ax1.set_ylim([-lim1, lim1])
 ax1.set_zlim([-lim1, lim1])
-# Hide everything
+ax3.set_title('Inertial frame', fontdict={'size': 15})
+lim3 = np.max(np.abs(w_inertial))
+ax3.set_xlim([-lim3, lim3])
+ax3.set_ylim([-lim3, lim3])
+ax3.set_zlim([-lim3, lim3])
+# Style axes
+val1 = [lim1 * 1.5, 0, 0]
+val3 = [lim3 * 1.5, 0, 0]
+labels = ['X', 'Y', 'Z']
+for i in range(3):
+    xyz = [-val1[i-0], -val1[i-1], -val1[i-2]]
+    uvw = [2 * val1[i-0], 2 * val1[i-1], 2 * val1[i-2]]
+    ax1.quiver(*xyz, *uvw, color='k', arrow_length_ratio=.05)
+    ax1.text(val1[i-0], val1[i-1], val1[i-2], labels[i], fontsize=15)
+    ax3.quiver(*xyz, *uvw, color='k', arrow_length_ratio=.05)
+    ax3.text(val3[i-0], val3[i-1], val3[i-2], labels[i], fontsize=15)
+# Hide everything default
 ax1.xaxis.set_pane_color((1, 1, 1, 0))
 ax1.yaxis.set_pane_color((1, 1, 1, 0))
 ax1.zaxis.set_pane_color((1, 1, 1, 0))
 ax1._axis3don = False
+ax3.xaxis.set_pane_color((1, 1, 1, 0))
+ax3.yaxis.set_pane_color((1, 1, 1, 0))
+ax3.zaxis.set_pane_color((1, 1, 1, 0))
+ax3._axis3don = False
 
-# Axis 2
+# Axes 2 and 4
+ax2.set_xlabel('t (s)')
+ax2.set_ylabel('$\omega$ (rad/s)')
 minw = np.min(w)
 maxw = np.max(w)
 ax2.set_xlim([0, t[-1]])
 ax2.set_ylim([minw - .1 * np.abs(minw), maxw + .1 * np.abs(maxw)])
+ax4.set_xlabel('t (s)')
+ax4.set_ylabel('$\omega$ (rad/s)')
+minw_i = np.min(w_inertial)
+maxw_i = np.max(w_inertial)
+ax4.set_xlim([0, t[-1]])
+ax4.set_ylim([minw_i - .1 * np.abs(minw_i), maxw_i + .1 * np.abs(maxw_i)])
 
 # Animation
+angular_velocity, = ax1.plot([], [], [], color='b', lw=2.5, alpha=.6)
+angular_momentum, = ax1.plot([], [], [], color='r', lw=2.5, alpha=.6)
 ln1, = ax1.plot([], [], [], color='b', lw=1.2, label=r'$\vec{\omega}$')
 ln2, = ax1.plot([], [], [], color='r', lw=1.2, label=r'$\vec{L}$')
-angular_velocity = ax1.quiver([], [], [], [], [], [], color='b')
-angular_momentum = ax1.quiver([], [], [], [], [], [], color='r')
 omega1, = ax2.plot([], [], label=r'$\omega_1$')
 omega2, = ax2.plot([], [], label=r'$\omega_2$')
 omega3, = ax2.plot([], [], label=r'$\omega_3$')
+ax1.legend(loc='upper right')
+ax2.legend(loc='upper right')
+ax3.legend(loc='upper right')
+ax4.legend(loc='upper right')
 def func(i):
-    global angular_velocity
-    global angular_momentum
-    angular_velocity.remove()
-    angular_momentum.remove()
-    angular_velocity = ax1.quiver(0, 0, 0, *w[:, i], color='b', arrow_length_ratio=.1, lw=2.5, alpha=.6)
-    angular_momentum = ax1.quiver(0, 0, 0, *L[:, i], color='r', arrow_length_ratio=.1, lw=2.5, alpha=.6)
+    angular_velocity.set_data_3d(*zip(np.zeros(3), w[:, i]))
+    angular_momentum.set_data_3d(*zip(np.zeros(3), L[:, i]))
     ln1.set_data_3d(w[:, :i])
     ln2.set_data_3d(L[:, :i])
     omega1.set_data(t[:i], w[0, :i])
     omega2.set_data(t[:i], w[1, :i])
     omega3.set_data(t[:i], w[2, :i])
+    return \
+        angular_velocity, \
+        angular_momentum, \
+        ln1, \
+        ln2, \
+        omega1, \
+        omega2, \
+        omega3
 
-def init_func():
-    ax1.legend(loc='upper right')
-    ax2.legend(loc='upper right')
-    ax2.set_xlabel('t (s)')
-    ax2.set_ylabel('$\omega$ (rad/s)')
-    # Style axis one
-    val = [lim1 * 1.5, 0, 0]
-    labels = ['X', 'Y', 'Z']
-    for i in range(3):
-        xyz = [-val[i-0], -val[i-1], -val[i-2]]
-        uvw = [2 * val[i-0], 2 * val[i-1], 2 * val[i-2]]
-        ax1.quiver(*xyz, *uvw, color='k', arrow_length_ratio=.05)
-        ax1.text(val[i-0], val[i-1], val[i-2], labels[i], fontsize=15)
-
-ani = FuncAnimation(fig, func, frames=len(t), init_func=init_func, interval=50)
+ani = FuncAnimation(fig, func, frames=len(t), interval=50, blit=True)
 ani.save('rb_rotation.gif', writer='pillow', fps=50, dpi=100)
 
 plt.show()
-
-# Problems
-# w1, w2 = 0
-# w1, w3 = 0
-# w3 = 0
